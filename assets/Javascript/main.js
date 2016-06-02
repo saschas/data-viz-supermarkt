285594f17d75abefeca1da1470bf1e0fcecd4f62
anni1994 <annikawiedera@Annikas-MacBook-Pro.local>
2016-06-02 12:14:20 +0200

    zweite Grafik hoher bubbre

--- assets/Javascript/main.js
+++ assets/Javascript/main.js
@@ -1,7 +1,17 @@
+/*--------------Einkaufswagen erste Grafik Anfang-------------*/
 $('#Einkaufswagen').hover(function() {
 	$('#Einkaufswagen-Sprechblase').css('visibility', 'visible');
-	$('#Einkaufswagen-color').find('path, rect').css('fill', 'tomato');
+	$('#Einkaufswagen-color').find('path, rect').css('fill', '#E30613');
 }).mouseleave(function() {
 	$('#Einkaufswagen-Sprechblase').css('visibility', 'hidden');
 	$('#Einkaufswagen-color').find('path, rect').css('fill', '#27348B');
 });
+/*--------------Einkaufswagen erste Grafik Ende-------------*/
+/*--------------Verderben zweite Grafik Anfang-------------*/
+$('#faul').hover(function() {
+	$('#faul-Sprechblase').css('visibility', 'visible');
+}).mouseleave(function() {
+	$('#faul-Sprechblase').css('visibility', 'hidden');
+});
+
+/*--------------Verderben zweite Grafik Anfang-------------*/
